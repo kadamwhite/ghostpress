8b36ccdc9644cb95715636aa96f07b1b636ab9ac
K. Adam White <kadamwhite@users.noreply.github.com>
2016-01-20 15:57:10 -0500

    Add route for individual content pages

--- server/routes.js
+++ server/routes.js
@@ -3,38 +3,38 @@
 
 Ghost Context Table, from http://themes.ghost.org/docs/context-overview
 
-Context | URL                   | Template          | Data          | Body Classes
---------+-----------------------+-------------------+---------------+------------------
-index,  | /                     | home.hbs,         | [{post}],     | .home-template
-home    |                       | index.hbs         | {pagination}  |
---------+-----------------------+-------------------+---------------+------------------
-index,  | /page/2/              | index.hbs         | [{post}],     | .paged
-paged   |                       |                   | {pagination}  |
---------+-----------------------+-------------------+---------------+------------------
-post    | /:permalink/          | post-:slug.hbs,   | {post}        | .post-template,
-        |                       | post.hbs          |               | .tag-:slug
---------+-----------------------+-------------------+---------------+------------------
-page    | /:slug/               | page-:slug.hbs,   | {post}        | .page-template,
-        |                       | page.hbs,         |               | .tag-:slug
-        |                       | post.hbs          |               |
---------+-----------------------+-------------------+---------------+------------------
-tag     | /tag/:slug/           | tag-:slug.hbs,    | [{post}],     | .tag-template,
-        |                       | tag.hbs,          | {pagination}, | .tag-:slug
-        |                       | index.hbs         | {tag}         |
---------+-----------------------+-------------------+---------------+------------------
-tag,    | /tag/:slug/page/2/    | tag-:slug.hbs,    | [{post}],     | .tag-template,
-paged   |                       | tag.hbs or        | {pagination}, | .tag-:slug,
-        |                       | index.hbs         | {tag}         | .paged
---------+-----------------------+-------------------+---------------+------------------
-author  | /author/:slug/        | author-:slug.hbs, | [{post}],     | .author-template,
-        |                       | author.hbs or     | {pagination}, | .author-:slug
-        |                       | index.hbs         | {author}      |
---------+-----------------------+-------------------+---------------+------------------
-author, | /author/:slug/page/2/ | author-:slug.hbs, | [{post}],     | .author-template,
-paged   |                       | author.hbs,       | {pagination}, | .author-:slug
-        |                       | index.hbs         | {author}      | .paged
---------+-----------------------+-------------------+---------------+------------------
-private | /private/             | private.hbs       | {error}       | .private-template
+Context | URL                   | Template          | Data          | Body Classes      | Done?
+--------+-----------------------+-------------------+---------------+-------------------+-------
+index,  | /                     | home.hbs,         | [{post}],     | .home-template    | Yes
+home    |                       | index.hbs         | {pagination}  |                   |
+--------+-----------------------+-------------------+---------------+-------------------+-------
+index,  | /page/2/              | index.hbs         | [{post}],     | .paged            | Yes
+paged   |                       |                   | {pagination}  |                   |
+--------+-----------------------+-------------------+---------------+-------------------+-------
+post    | /:permalink/          | post-:slug.hbs,   | {post}        | .post-template,   | Yes
+        |                       | post.hbs          |               | .tag-:slug        |
+--------+-----------------------+-------------------+---------------+-------------------+-------
+page    | /:slug/               | page-:slug.hbs,   | {post}        | .page-template,   | Yes
+        |                       | page.hbs,         |               | .tag-:slug        |
+        |                       | post.hbs          |               |                   |
+--------+-----------------------+-------------------+---------------+-------------------+-------
+tag     | /tag/:slug/           | tag-:slug.hbs,    | [{post}],     | .tag-template,    |
+        |                       | tag.hbs,          | {pagination}, | .tag-:slug        |
+        |                       | index.hbs         | {tag}         |                   |
+--------+-----------------------+-------------------+---------------+-------------------+-------
+tag,    | /tag/:slug/page/2/    | tag-:slug.hbs,    | [{post}],     | .tag-template,    |
+paged   |                       | tag.hbs or        | {pagination}, | .tag-:slug,       |
+        |                       | index.hbs         | {tag}         | .paged            |
+--------+-----------------------+-------------------+---------------+-------------------+-------
+author  | /author/:slug/        | author-:slug.hbs, | [{post}],     | .author-template, |
+        |                       | author.hbs or     | {pagination}, | .author-:slug     |
+        |                       | index.hbs         | {author}      |                   |
+--------+-----------------------+-------------------+---------------+-------------------+-------
+author, | /author/:slug/page/2/ | author-:slug.hbs, | [{post}],     | .author-template, |
+paged   |                       | author.hbs,       | {pagination}, | .author-:slug     |
+        |                       | index.hbs         | {author}      | .paged            |
+--------+-----------------------+-------------------+---------------+-------------------+-------
+private | /private/             | private.hbs       | {error}       | .private-template | N/A
 
 */
 
@@ -64,11 +64,11 @@ router.use(function( req, res, next ) {
 
 router.get( '/', function homepageRoute( req, res, next ) {
   var postsPromise = wp.posts().perPage( 10 ).embed();
-  res.locals.context = [ 'index', 'home' ];
   bluebird.props({
     meta_title: pageTitle(),
     posts: postsPromise,
     pagination: postsPromise.then( getPaginationObj ),
+    context: [ 'index', 'home' ],
     body_class: 'home-template'
   }).then(function( context ) {
     context.posts = context.posts.map( decease.post );
@@ -78,11 +78,11 @@ router.get( '/', function homepageRoute( req, res, next ) {
 
 router.get( '/page/:pagenum', function pagedArchiveRoute( req, res, next ) {
   var postsPromise = wp.posts().perPage( 10 ).page( req.params.pagenum ).embed();
-  res.locals.context = [ 'index', 'paged' ];
   bluebird.props({
     meta_title: pageTitle( 'Page ' + req.params.page ),
     posts: postsPromise,
     pagination: postsPromise.then( getPaginationObj ),
+    context: [ 'index', 'paged' ],
     body_class: 'paged'
   }).then(function( context ) {
     context.posts = context.posts.map( decease.post );
@@ -90,22 +90,41 @@ router.get( '/page/:pagenum', function pagedArchiveRoute( req, res, next ) {
   }).catch( next );
 });
 
-router.get( '/:slug', function homepageRoute( req, res, next ) {
+router.get( '/:slug', function singlePageRoute( req, res, next ) {
   var postsPromise = wp.posts().name( req.params.slug ).embed().then(function( posts ) {
     // posts().name() returns an array, hopefully with only one element
     return posts.length === 1 ? decease.post( posts[ 0 ] ) : 404;
   });
-  var pageTitlePromise = postsPromise.then(function( post ) {
+  var postTitlePromise = postsPromise.then(function( post ) {
     return pageTitle( post !== 404 ? post.title : '404' );
   });
-  res.locals.context = [ 'post' ];
   bluebird.props({
-    meta_title: pageTitlePromise,
+    meta_title: postTitlePromise,
     post: postsPromise,
+    context: [ 'post' ],
     body_class: 'post-template'
   }).then(function( context ) {
     if ( context.post === 404 ) { return next(); }
     res.render( 'post', context );
+  }).catch( next );
+});
+
+// If we slip past the post slug routes, try to retrieve a matching page instead
+router.get( '/:pageslug', function pageRoute( req, res, next ) {
+  var pagePromise = wp.pages().name( req.params.pageslug ).embed().then(function( pages ) {
+    return pages.length === 1 ? decease.post( pages[ 0 ] ) : 404;
+  });
+  var pageTitlePromise = pagePromise.then(function( page ) {
+    return pageTitle( page !== 404 ? page.title : '404' );
+  });
+  bluebird.props({
+    meta_title: pageTitlePromise,
+    post: pagePromise,
+    context: [ 'page' ],
+    body_class: 'page-template'
+  }).then(function( context ) {
+    if ( context.post === 404 ) { return next(); }
+    res.render( 'page', context );
   }).catch( next );
 });
 
